7f36e79ca3f68eaf0e3654cc021fb8e42339ab75
Cristian Lazar <lcri9530@gmail.com>
2021-08-30 10:32:14 +0300

    update readme.md 5

--- writeups/re/speed/solve.py
+++ writeups/re/speed/solve.py
@@ -10,8 +10,6 @@ a = libc.rand()
 b = libc.rand()
 print(a, b)
 
-for c in range(0, 99999999999):
-    if b == (c ^ a ^ 1337):
-        print(c)
-        print("CTF{%s}" % hashlib.sha256(c.encode()).hexdigest())
-        exit(0)
+c = b ^ a ^ 1337
+print(c)
+print("CTF{%s}" % hashlib.sha256(str(c).encode()).hexdigest())
